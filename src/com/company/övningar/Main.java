8a83e3113e6ee477b0a06263133f6cbaa2221c48
Jessica Feder <jessica.feder@student.kyh.se>
2020-09-29 11:30:06 +0200

    nya kommentarer

--- src/com/company/övningar/Main.java
+++ src/com/company/övningar/Main.java
@@ -2,14 +2,20 @@ package com.company.övningar;
 
 public class Main {
     public static void main(String[] args) {
+
         int width = Math.toIntExact(Math.round(Math.random()* 9 + 1));
-        int height = Math.toIntExact(Math.round(Math.random()* 9 + 1));
+
+
+        double r = Math.random() * 9+1;
+        System.out.println(r);
+        long r2 = Math.round(r);
+        System.out.println(r2);
+        int height = Math.toIntExact(r2);
+        System.out.println(height);
 
         Shape rect = new Rectangle(width, height);
-        System.out.println(rect.getArea() + " " + rect.getCircumference());
+        //System.out.println(rect.getArea() + " " + rect.getCircumference());
         //System.out.println(trian.getArea() + " " + trian.getCircumference());*/
-//verkar cäll funka?
-        // jajamän!
     }
 }
 
