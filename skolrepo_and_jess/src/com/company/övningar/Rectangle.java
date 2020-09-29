648165eaf2e85f11a568e63a0c3afc861a4755d9
jessicafeder <56555778+jessicafeder@users.noreply.github.com>
2020-09-29 10:05:16 +0200

    Add files via upload

---
+++ skolrepo_and_jess/src/com/company/övningar/Rectangle.java
@@ -0,0 +1,19 @@
+package com.company.övningar;
+
+public class Rectangle implements Shape{
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getArea(){
+        return width*height;
+    }
+
+    public int getCircumference(){
+        return 2*width + 2*height;
+    }
+}
