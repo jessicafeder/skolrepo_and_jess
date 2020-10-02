9970b2f392c10f22e4c4359b1d4759b20ba7ef20
Andreas Berg <andreas.berg2@student.kyh.se>
2020-10-02 13:36:38 +0200

    typ färdiga kalkylator och tester

--- src/com/company/Calculator/Calculator.java
+++ src/com/company/Calculator/Calculator.java
@@ -3,10 +3,26 @@ package com.company.Calculator;
 import java.util.Scanner;
 
 public class Calculator {
-    public Calculator() {
+    public int add(int a, int b) {
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+
+    public int div(int a, int b) {
+        return a/b;
+    }
+
+    public int mod(int a, int b) {
+        return a%b;
+    }
+
+
+    public static void main(String[] args) {
         int a, b;
         String method;
-
+        Calculator calc = new Calculator();
         System.out.println("Welcome to the Calculator!");
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()) {
@@ -14,18 +30,35 @@ public class Calculator {
             method = sc.next();
             b = sc.nextInt();
             if(method.equals("+")) {
-                System.out.println("The answer is: " + add(a,b));
-            } else {
+                System.out.println("The answer is: " + calc.add(a,b));
+
+            }else if(method.equals("-")){
+                System.out.println("The answer is: " + calc.sub(a,b));
+
+            }else if(method.equals("%")) {
+                System.out.println("The answer is: " + calc.mod(a,b));
+
+            }else if(method.equals("/")){
+                System.out.println("The answer is: " + calc.div(a,b));
+
+            } else if (method.equals(">")) {
+                System.out.println("The answer is: " + Math.max(a,b));
+
+            } else if (method.equals("<")) {
+                System.out.println("The answer is: " + Math.min(a, b));
+            }
+            else {
                 System.out.println("The method of calculation is not supported yet");
             }
+
         }
+
     }
 
-    public int add(int a, int b) {
-        return a-b;
-    }
-
-    public static void main(String[] args) {
-        new Calculator();
+    public int max(int a, int b) {
+        if(a > b) {
+            return a;
+        }else
+            return b;
     }
 }
