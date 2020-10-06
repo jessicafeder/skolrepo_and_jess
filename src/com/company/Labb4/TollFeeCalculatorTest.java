560cca24927014036e254b7c9b98afd7cb405375
Jessica Feder <jessica.feder@student.kyh.se>
2020-10-06 08:25:21 +0200

    Lite test

--- src/com/company/Labb4/TollFeeCalculatorTest.java
+++ src/com/company/Labb4/TollFeeCalculatorTest.java
@@ -1,10 +1,10 @@
 package com.company.Labb4;
+import java.io.OptionalDataException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 
 @DisplayName("Testing isTollFreeDate")
 public class TollFeeCalculatorTest{
@@ -13,6 +13,16 @@ public class TollFeeCalculatorTest{
         LocalDateTime date = LocalDateTime.parse("2020-06-01 10:00", DateTimeFormatter.ofPattern("yyyy-MM-DD HH:MM"));
         assertTrue(TollFeeCalculator.isTollFreeDate(date));
     }
+
+@DisplayName("Testing Array")
+    @Test
+    void TollFeeCalculator(){
+        OptionalDataException dateStrings = null;
+        LocalDateTime[] dates = new LocalDateTime[dateStrings.length];
+    assertEquals("The array length is not what was expected!", (long) array.length - 1, (long) modifiedArray.length);
+
+    }
+
 }
 
 // vi kan köra ungefär samma som ovan till att testa bugg nr 1 som är math.max
